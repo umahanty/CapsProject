185349f43fb2307afe1430d645795a037a8432c9
umahanty <uditnarayanmahanty911@gmail.com>
2024-05-24 20:51:03 +0530

    commit 1

--- src/main/java/com/project/mentorbabaa/insureme/PolicyController.java
+++ src/main/java/com/project/mentorbabaa/insureme/PolicyController.java
@@ -1,32 +1,31 @@
 package com.project.mentorbabaa.insureme;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PolicyController {
-	
+
 	@Autowired
 	PolicyService policyService;
-	
-	
+
+
 	@GetMapping("/hello")
 	public String sayHello() {
 		return "hello";
 	}
-	
+
 	@GetMapping("/createPolicy")
 	public Policy createPolicy() {
-		
 		return policyService.CreatePolicy();
-		
-		
 	}
-	
+
 	@PostMapping("/createPolicy")
 	public Policy createPolicy(@RequestBody Policy policy) {
 		if(policy!=null) {
@@ -39,6 +38,20 @@ public class PolicyController {
 	public Policy getPolicyDetails(@PathVariable(value="policyId") int policyId) {
 		return policyService.getPolicyDetails(policyId);
 	}
-	
-}
 
+	@PutMapping("/updatePolicy/{policyId}")
+	public Policy updatePolicy(@PathVariable(value="policyId") int policyId, @RequestBody Policy policy) {
+		return policyService.updatePolicy(policyId, policy);
+	}
+
+	@GetMapping("/viewPolicy/{policyId}")
+	public Policy viewPolicy(@PathVariable(value="policyId") int policyId) {
+		return policyService.getPolicyDetails(policyId);
+	}
+
+	@DeleteMapping("/deletePolicy/{policyId}")
+	public String deletePolicy(@PathVariable(value="policyId") int policyId) {
+		policyService.deletePolicy(policyId);
+		return "Policy deleted successfully";
+	}
+}
--- src/main/java/com/project/mentorbabaa/insureme/PolicyService.java
+++ src/main/java/com/project/mentorbabaa/insureme/PolicyService.java
@@ -1,52 +1,57 @@
 package com.project.mentorbabaa.insureme;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class PolicyService {
-	
+
 	@Autowired
 	PolicyRepository policyRepository;
-	
-	
+
 	public Policy CreatePolicy() {
-		
 		Policy policy = generateDummyPolicy();
-		
 		return policyRepository.save(policy);
-		
-			
 	}
-	
-	public Policy updatePolicy() {
-		
+
+	public Policy updatePolicy(int policyId, Policy updatedPolicy) {
+		Optional<Policy> policy = policyRepository.findById(policyId);
+		if (policy.isPresent()) {
+			Policy existingPolicy = policy.get();
+			existingPolicy.setPolicyHolderName(updatedPolicy.getPolicyHolderName());
+			existingPolicy.setPolicyType(updatedPolicy.getPolicyType());
+			existingPolicy.setPolicyAmount(updatedPolicy.getPolicyAmount());
+			existingPolicy.setStartDate(updatedPolicy.getStartDate());
+			existingPolicy.setEndDate(updatedPolicy.getEndDate());
+			return policyRepository.save(existingPolicy);
+		}
 		return null;
 	}
-	
-	public Policy deletePolicy() {
-		
+
+	public Policy deletePolicy(int policyId) {
+		Optional<Policy> policy = policyRepository.findById(policyId);
+		if (policy.isPresent()) {
+			policyRepository.deleteById(policyId);
+			return policy.get();
+		}
 		return null;
 	}
-	
-	public Policy searchPolicy() {
-		
-		return null;
+
+	public Policy searchPolicy(int policyId) {
+		return policyRepository.findById(policyId).get();
 	}
-	
-	
+
 	public Policy generateDummyPolicy() {
 		return new Policy(1, "Shubham", "Individual" , 10000, "10-Sep-2021", "10-Sep-2022");
 	}
 
 	public Policy registerPolicy(Policy policy) {
-		// TODO Auto-generated method stub
 		return policyRepository.save(policy);
 	}
 
 	public Policy getPolicyDetails(int policyId) {
-		// TODO Auto-generated method stub
 		return policyRepository.findById(policyId).get();
 	}
 
